afdbeb1770d2bd3ed65f3d2b9b80a8d9d723f8ac
MaiJGitHub <jmai112302@gmail.com>
2023-08-09 01:40:35 +0000

    update 3

--- main.js
+++ main.js
@@ -1,92 +1,244 @@
-class Card {
-    constructor(color, value) {
-        this.color = color;
-        this.value = value;
-    }
+kaboom({
+	background: [74, 48, 82],
+})
+
+loadSprite("bag", "./sprites/bag.png")
+loadSprite("ghosty", "./sprites/ghosty.png")
+loadSprite("grass", "./sprites/grass.png")
+loadSprite("steel", "./sprites/steel.png")
+loadSprite("door", "./sprites/door.png")
+loadSprite("key", "./sprites/key.png")
+loadSprite("bean", "./sprites/bean.png")
+loadSprite("pineapple", "./sprites/pineapple.png")
+
+scene("main", (levelIdx) => {
+
+	const SPEED = 320
+
+	// character dialog data
+	const characters = {
+		"a": {
+			sprite: "bag",
+			msg: "Hi Dav! Welcome to your own dungeon, your goal is to find the key. The controls are w a s d to move, good luck and be careful! ;) ",
+		},
+		"b": {
+			sprite: "ghosty",
+			msg: "Who are you? You can see me??",
+		},
+	}
+
+	// level layouts
+	const levels = [
+		[	
+			"                       ",
+			"       =======|========",
+			"       =              =",
+			"       =            = =",
+			"       =  =     =     =",
+			"       =         $    =",
+			"       =     =        =",
+			"       =        =     =",
+			"       =   =          =",
+			"       =           =  =",
+			"       =    a @       =",
+			"       = =            =",
+			"       =         =    =",
+			"       ================",
+		],
+		[
+			"       ----------------",
+			"       - $            -",
+			"       -           -  -",
+			"       |   -          -",
+			"       -       -      -",
+			"       -         -    -",
+			"       - -            -",
+			"       -      @  b -  -",
+			"       -    -         -",
+			"       ----------------",
+		],
+	]
+
+	const level = addLevel(levels[levelIdx], {
+		tileWidth: 64,
+		tileHeight: 64,
+		pos: vec2(64, 64),
+		tiles: {
+			"=": () => [
+				sprite("grass"),
+				area(),
+				body({ isStatic: true }),
+				anchor("center"),
+			],
+			"-": () => [
+				sprite("steel"),
+				area(),
+				body({ isStatic: true }),
+				anchor("center"),
+			],
+			"$": () => [
+				sprite("key"),
+				area(),
+				anchor("center"),
+				"key",
+			],
+			"@": () => [
+				sprite("bean"),
+				area(),
+				body(),
+				anchor("center"),
+				"player",
+			],
+			"|": () => [
+				sprite("door"),
+				area(),
+				body({ isStatic: true }),
+				anchor("center"),
+				"door",
+			],
+		},
+		// any() is a special function that gets called everytime there's a
+		// symbole not defined above and is supposed to return what that symbol
+		// means
+		wildcardTile(ch) {
+			const char = characters[ch]
+			if (char) {
+				return [
+					sprite(char.sprite),
+					area(),
+					body({ isStatic: true }),
+					anchor("center"),
+					"character",
+					{ msg: char.msg },
+				]
+			}
+		},
+	})
+
+	// get the player game obj by tag
+	const player = level.get("player")[0]
+
+	function addDialog() {
+		const h = 160
+		const pad = 16
+		const bg = add([
+			pos(0, height() - h),
+			rect(width(), h),
+			color(0, 0, 0),
+			z(100),
+		])
+		const txt = add([
+			text("", {
+				width: width(),
+			}),
+			pos(0 + pad, height() - h + pad),
+			z(100),
+		])
+		bg.hidden = true
+		txt.hidden = true
+		return {
+			say(t) {
+				txt.text = t
+				bg.hidden = false
+				txt.hidden = false
+			},
+			dismiss() {
+				if (!this.active()) {
+					return
+				}
+				txt.text = ""
+				bg.hidden = true
+				txt.hidden = true
+			},
+			active() {
+				return !bg.hidden
+			},
+			destroy() {
+				bg.destroy()
+				txt.destroy()
+			},
+		}
+	}
+	
+	function addFlamebar(position = vec2(0), angle = 0, num = 6) {
+
+	// Create a parent game object for position and rotation
+	const flameHead = add([
+		pos(position),
+		rotate(angle),
+	])
+
+	// Add each section of flame as children
+	for (let i = 0; i < num; i++) {
+		flameHead.add([
+			sprite("pineapple"),
+			pos(0, i * 48),
+			area(),
+			anchor("center"),
+			"flame",
+		])
+	}
+
+	// The flame head's rotation will affect all its children
+	flameHead.onUpdate(() => {
+		flameHead.angle += dt() * 60
+	})
+
+	return flameHead
+
 }
 
-class Player {
-    constructor(id) {
-        this.id = id;
-        this.hand = [];
-    }
+	addFlamebar(vec2(200, 300), -60)
+	
+	let hasKey = false
+	const dialog = addDialog()
 
-    drawCard(card) {
-        this.hand.push(card);
-    }
+	player.onCollide("key", (key) => {
+		destroy(key)
+		hasKey = true
+	})
 
-    playCard(card) {
-        const index = this.hand.findIndex(c => c.color === card.color && c.value === card.value);
-        if (index !== -1) {
-            this.hand.splice(index, 1);
-            return true;
-        }
-        return false;
-    }
-}
+	player.onCollide("door", () => {
+		if (hasKey) {
+			if (levelIdx + 1 < levels.length) {
+				go("main", levelIdx + 1)
+			} else {
+				go("win")
+			}
+		} else {
+			dialog.say("Wheres your keys?!")
+		}
+	})
 
-class UnoGame {
-    constructor() {
-        this.colors = ['red', 'green', 'blue', 'yellow'];
-        this.values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'skip', 'reverse', 'draw2'];
-        this.drawPile = [];
-        this.discardPile = [];
-        this.players = [];
-        this.currentPlayerIndex = 0; // Added to track the current player's index
-        this.direction = 1; // 1 for clockwise, -1 for counterclockwise
+	// talk on touch
+	player.onCollide("character", (ch) => {
+		dialog.say(ch.msg)
+	})
 
-        this.initialize();
-        this.dealInitialHands();
-        this.startGame();
-    }
+	const dirs = {
+		"left": LEFT,
+		"right": RIGHT,
+		"up": UP,
+		"down": DOWN,
+	}
 
-    initialize() {
-        this.createDrawPile();
-        this.shuffleDrawPile(); // Call the shuffle method to actually shuffle the draw pile
-    }
+	for (const dir in dirs) {
+		onKeyPress(dir, () => {
+			dialog.dismiss()
+		})
+		onKeyDown(dir, () => {
+			player.move(dirs[dir].scale(SPEED))
+		})
+	}
 
-    dealInitialHands() {
-        for (let i = 0; i < 7; i++) {
-            for (const player of this.players) {
-                const card = this.drawPile.pop();
-                player.drawCard(card);
-            }
-        }
-    }
+})
 
-    createDrawPile() {
-        // Create a deck of cards and add them to the draw pile
-        for (const color of this.colors) {
-            for (const value of this.values) {
-                this.drawPile.push(new Card(color, value));
-            }
-        }
-    }
+scene("win", () => {
+	add([
+		text("You Survived!"),
+		pos(width() / 2, height() / 2),
+		anchor("center"),
+	])
+})
 
-    shuffleDrawPile() {
-        // Move the shuffleDeck function out of the method and make it static
-        UnoGame.shuffleDeck(this.drawPile);
-    }
-
-    static shuffleDeck(deck) {
-        for (let i = deck.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [deck[i], deck[j]] = [deck[j], deck[i]];
-        }
-    }
-
-    startGame() {
-        console.log("Uno game started!");
-        // Add your game logic here to manage turns and gameplay
-    }
-
-    // Add the rest of your methods here to implement gameplay
-
-    // For brevity, I'm omitting the rest of the methods needed for Uno gameplay.
-}
-
-// Create two players and start the game
-const player1 = new Player(1);
-const player2 = new Player(2);
-const unoGame = new UnoGame();
-unoGame.players = [player1, player2];
+go("main", 0)
