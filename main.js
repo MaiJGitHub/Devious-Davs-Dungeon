ab82dad23719a82fec6cf495782627e9e8066460
MtLight <tkory85@gmail.com>
2023-08-10 13:55:42 +0000

    commit

--- main.js
+++ main.js
@@ -11,7 +11,54 @@ loadSprite("key", "./sprites/key.png")
 loadSprite("man", "./sprites/Witch.png")
 loadSprite("box", "./sprites/Box2.png")
 loadSprite("spooky", "./sprites/Box2.png")
-loadSprite("gif", "./sprites/jumpscare-agatha-dark-deception-d54spbdyxgggqy9c.gif")
+// loadSprite("gif", "./sprites/Box2.png")
+
+loadSprite("frame1", "./bonnie/ezgif-frame-001.jpg")
+loadSprite("frame2", "./bonnie/ezgif-frame-002.jpg")
+loadSprite("frame3", "./bonnie/ezgif-frame-003.jpg")
+loadSprite("frame4", "./bonnie/ezgif-frame-004.jpg")
+loadSprite("frame5", "./bonnie/ezgif-frame-005.jpg")
+loadSprite("frame6", "./bonnie/ezgif-frame-006.jpg")
+loadSprite("frame7", "./bonnie/ezgif-frame-007.jpg")
+loadSprite("frame8", "./bonnie/ezgif-frame-008.jpg")
+loadSprite("frame9", "./bonnie/ezgif-frame-009.jpg")
+loadSprite("frame10", "./bonnie/ezgif-frame-010.jpg")
+loadSprite("frame11", "./bonnie/ezgif-frame-011.jpg")
+loadSprite("frame12", "./bonnie/ezgif-frame-012.jpg")
+loadSprite("frame13", "./bonnie/ezgif-frame-013.jpg")
+loadSprite("frame14", "./bonnie/ezgif-frame-014.jpg")
+loadSprite("frame15", "./bonnie/ezgif-frame-015.jpg")
+loadSprite("frame16", "./bonnie/ezgif-frame-016.jpg")
+
+// loadSprite('jumpscare', {
+//     anims: {
+//         myAnimation: ['frame1', 'frame2', 'frame3','frame4', 'frame5', 'frame6','frame7', 'frame8', 'frame9', 'frame10', 'frame11', 'frame12', 'frame13', 'frame14', 'frame15', 'frame16']
+//     }
+// })
+
+loadSprite("jumpscare", [
+    "./bonnie/ezgif-frame-001.jpg",
+    "./bonnie/ezgif-frame-002.jpg",
+   "./bonnie/ezgif-frame-003.jpg",
+    "./bonnie/ezgif-frame-004.jpg",
+   "./bonnie/ezgif-frame-005.jpg",
+    "./bonnie/ezgif-frame-006.jpg",
+     "./bonnie/ezgif-frame-007.jpg",
+      "./bonnie/ezgif-frame-008.jpg",
+       "./bonnie/ezgif-frame-009.jpg",
+        "./bonnie/ezgif-frame-010.jpg",
+         "./bonnie/ezgif-frame-011.jpg",
+          "./bonnie/ezgif-frame-012.jpg",
+           "./bonnie/ezgif-frame-013.jpg",
+            "./bonnie/ezgif-frame-014.jpg",
+             "./bonnie/ezgif-frame-015.jpg",
+              "./bonnie/ezgif-frame-016.jpg",
+], {
+    anims: {
+        run: { from: 1, to: 15 },
+    },
+});
+
 
 scene("intro", () => {
 	
@@ -35,36 +82,53 @@ scene("main", (levelIdx) => {
 	// level layouts
 	const levels = [
 		[	
-			"                              ",
-			"                              ",
-			"         ========|======",
-			"         =             =",
-			"         =   *  *    $ =",
-			"         =  *          =",
-			"         =         #   =",
-			"         =  a @        =",
-			"         =    *        =",
-			"         =         *   =",
-			"         ===============",
+			"                      ",
+			"                      ",
+			"       ========|======",
+			"       =             =",
+			"       =   *  *    $ =",
+			"       =  *          =",
+			"       =         #   =",
+			"       =  a @        =",
+			"       =    *        =",
+			"       =         *   =",
+			"       ===============",
 		],
 		[	
-			"                                         ",
-			"                                         ",
-			"         =====================|==",
-			"         =*                * =  =",
-			"         =                   =  =",
-			"         =  ==================  =",
-			"         =                      =",
-			"         =                      =",
-			"         =  ====  ====  =========",
-			"         =  =     =     = $ = # =",
-			"         =  =     =     =   =   =",
-			"         =  = ===== =====   =   =",
-			"         =  = @ =*  =   a   =   =",
-			"         = *=   =               =",
-			"         =  =a  =               =",
-			"         ========================",
+			"                              ",
+			"      =====================|==",
+			"      =*                * =  =",
+			"      =                   =  =",
+			"      =  ==================  =",
+			"      =                      =",
+			"      =                      =",
+			"      =  ====  ====  =========",
+			"      =  =     =     = $ = # =",
+			"      =  =     =     =   =   =",
+			"      =  = ===== =====   =   =",
+			"      =  = @ =*  =   a   =   =",
+			"      = *=   =               =",
+			"      =  =a  =               =",
+			"      ========================",
 		],
+		[	
+			"                              ",
+			"      ===========|============",
+			"      =#        =        *   =",
+			"      =         =            =",
+			"      =  ==================  =",
+			"      =         =     =      =",
+			"      =         =     =      =",
+			"      =======  ===== =====   =",
+			"      =        =      $    # =",
+			"      =        =         =   =",
+			"      =  = ===== =====   =   =",
+			"      =  = @ =*  =   a   =   =",
+			"      = *=   =      =        =",
+			"      =  =   = a    =        =",
+			"      ========================",
+		],
+	
 	]
 
 	const level = addLevel(levels[levelIdx], {
@@ -206,41 +270,82 @@ scene("main", (levelIdx) => {
 	})
 	
 	
+	// add([
+	//     sprite('mySprite'),
+	//     pos(80, 40),
+	//     area(),
+	//     play('myAnimation')
+	// ])
+	
+	const jumpscare = add([
+	    sprite("jumpscare"),
+	    pos(80, 80),
+	]);
+	
     player.onCollide("spooky", () => {
-		// const audio = new Audio('');
-  //      const audio2 = new Audio('');
+    	alert("jumpscare")
+    // 	 // get the sprite object
+    // const mySprite = level.get('jumpscare')[0];
+    // play the animation
+    jumpscare.play('run');
+		// document.querySelectorAll('audio, video').forEach((element) => {
+  //        element.muted = true;
+  //      });
+
+        // document.getElementById('jumpsacred').style.display = 'inline-block';
+
+   
+		// const audio = new Audio('XSCREAM.wav');
+  //      // const audio2 = new Audio('');
+  //      go("jumpsacred");
+
   //      audio.play();
 
-  //      setTimeout(() => {
-  //        document.getElementById('').style.display = 'none';
-  //        audio.pause();
-  //        document.getElementById('').style.display = 'inline-block';
-  //        audio2.play();
-  //        setTimeout(() => {
-  //          document.getElementById('').style.display = 'none';
-  //          document.getElementById('').style.display = 'inline-block';
-  //          setTimeout(() => {
-  //            window.location.href = "";
-  //          }, 8900);
-  //          //static for a few seconds then game over screen then reset to main menu
-  //        }, 2700);
-
-  //        //static for a few seconds then game over screen then reset to main menu
-  //      }, 900);
+        // setTimeout(() => {
+        //   //document.getElementById('jumpsacred').style.display = 'none';
+        //   //audio.pause();
+        //   //static for a few seconds then game over screen then reset to main menu
+          
+          
+        // }, 900);
 	})
-	let jumpscareTriggered = false;
+	// let jumpscareTriggered = false;
 	
-	player.onCollide("spooky", () => {
-        if (!jumpscareTriggered) {
-            jumpscareTriggered = true;
-            const jumpscareEntity = add([
-                sprite("gif"), // Replace with your actual jumpscare GIF sprite name
-                pos(player.pos), // Display the jumpscare at the player's position
-                anchor("center"),
-                scale(1.5), // Adjust scale as needed
-            ]);
-        }
-    });
+	// player.onCollide("spooky", () => {
+ //       if (!jumpscareTriggered) {
+ //           jumpscareTriggered = true;
+ //           const jumpscareEntity = add([
+ //               sprite("gif"), // Replace with your actual jumpscare GIF sprite name
+ //               pos(player.pos), // Display the jumpscare at the player's position
+ //               anchor("center"),
+ //               scale(1.5), // Adjust scale as needed
+ //           ]);
+ //       }
+ //   });
+    
+ //   function playVideo() {
+	//     const videoElement = document.getElementById("jumpsacred");
+	//     videoElement.play();
+	// }
+    
+ //   scene("jumpsacred", () => {
+	// 	const audio = new Audio('XSCREAM.wav');
+ //       // const audio2 = new Audio('');
+ //       audio.play();
+ //        playVideo();
+  
+	// 	  const backgroundImage = add([
+	// 	    sprite("gif"),
+	// 	    ]);
+		    
+	// 	  //setTimeout(() => {
+	// 	  //	go("instructions");
+ //   //         audio.pause();
+ //   //       //static for a few seconds then game over screen then reset to main menu
+ //   //       }, 900);
+		    
+	// });
+
 	
 
 	// talk on touch
